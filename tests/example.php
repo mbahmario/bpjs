8ea6d903dff923983bd07f7ee54ba7cd4f4c8c69
RIO IRMAYANTO <rio.irmayanto@gmail.com>
2025-06-03 03:59:10 +0700

    Update example.php

--- tests/example.php
+++ tests/example.php
@@ -12,32 +12,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 //     'service_name' => 'vclaim-rest'
 // ];
 
-$config_vclaim = [
-    'cons_id' =>  18036,
-    'secret_key' => '3wCA1C66C0',
-    'user_key' =>  '88417e5de63ecbe9d90650ca106cd925',
-    'base_url' => 'https://apijkn.bpjs-kesehatan.go.id',
-    'service_name' => 'vclaim-rest'
-];
-
-// $config_antrean = [
-//     'cons_id' =>  '18036',
-//     'secret_key' => '3wCA1C66C0',
-//     'user_key' =>  'ccf9c93fdaf305d62d0bf39f3e104682',
-//     'base_url' => 'https://apijkn.bpjs-kesehatan.go.id',
-//     'service_name' => 'antreanrs'
-// ];
-
-
 //use referensi serivce
 $referensi = new \Mbahmario\Bpjs\VClaim\Prb($config_vclaim);
-$keyword = '1017R002';
+$keyword = '123';
 $data =[];
 
-$query = $referensi->cariSRB('6334879','1017R0020525V003034');
+$query = $referensi->cariSRB('6334879','123');
 
 echo json_encode($query);
 // var_dump($referensi->cariByNoKartu('04','2023','CARD NUMBER','2'));
 
-//var_dump($referensi->cariByNoSuratKontrol('1017R0020323K001801'));
+//var_dump($referensi->cariByNoSuratKontrol('123'));
 
